f5b978024b5061ef975a516c33af125b684b1142
Bart van der Kruys <bartvanderkruys@gmail.com>
2017-01-01 20:26:41 +0100

    Implement quiz timer.

--- app/components/quiz-view.js
+++ app/components/quiz-view.js
@@ -5,15 +5,30 @@ export default Ember.Component.extend({
     store: Ember.inject.service(),
     quizStarted: false,
     canStartQuiz: null,
+    timeLeft: 120,
 
     init(){
         this._super();
     },
 
+    updateTimer(){
+        this.set("timeLeft", this.get("timeLeft") - 1);
+    },
+
+    startTimer: Ember.computed(function() {
+        let self = this;
+
+        setInterval(function(){
+            self.updateTimer();
+        }, 1000);
+    }),
+
     actions: {
         startQuiz() {
             this.get('onQuizStart')();
             this.set('quizStarted', true);
+            this.get('startTimer');
         }
-    }
+    },
+
 });
